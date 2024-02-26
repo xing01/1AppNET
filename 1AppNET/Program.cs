4e03d8d3ab6209c9c3bebb3b96ff800085a9c250
Lan Yang Villavicencio <leelanzing@hotmail.com>
2024-02-26 09:35:37 -0300

    basicos

--- 1AppNET/Program.cs
+++ 1AppNET/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! prueba");
+﻿//USO BÀSICO DE C# para NET
+using System;
+
+namespace Curso_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos de datos
+            int year = 10;
+            Console.WriteLine("Hello, World! prueba " + year);
+
+            string palabra = "gato";
+            double numDec = 2.5;
+            bool valor = false; //tipo de dato
+            Boolean valor2 = true; //estructura
+            Console.WriteLine("Hello, World! prueba {0} {1} {2}", palabra,valor,valor2);
+
+            Console.ReadLine();
+        }
+    }
+}
 

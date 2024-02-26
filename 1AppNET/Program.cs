603d0839e015d35c8aed1dd7ffe62ed1e6b63fcb
Lan Yang Villavicencio <leelanzing@hotmail.com>
2024-02-26 14:01:03 -0300

    triangulo de pascal

--- 1AppNET/Program.cs
+++ 1AppNET/Program.cs
@@ -1,4 +1,4 @@
-﻿//USO BÀSICO DE C# para NET
+﻿/*//USO BÀSICO DE C# para NET
 using System;
 
 namespace Curso_net_core
@@ -15,10 +15,20 @@ namespace Curso_net_core
             double numDec = 2.5;
             bool valor = false; //tipo de dato
             Boolean valor2 = true; //estructura
-            Console.WriteLine("Hello, World! prueba {0} {1} {2}", palabra,valor,valor2);
+            int v1 = 8;
+            int v2 = 2;
+            var suma = v1 + v2; // operadores aritmeticos
+            v1 %= v2; // operadores de asignación
+            Console.WriteLine("Hello, World! prueba {0} {1} {2}", suma,v1,valor2);
+
+            int data1 = 9;
+            int data2 = 3;
+            var data = data1 > data2; //operadores de comprobacion
+            Console.WriteLine("Resultado {0}", data);
+
 
             Console.ReadLine();
         }
     }
 }
-
+*/
---
+++ 1AppNET/TrianguloPascal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace _1AppNET
+{
+	class TrianguloPascal
+	{
+		static void Main(string[] args)
+		{
+			int pisos = 0;
+			int[] arreglo = new int[1];
+
+			//mostramos un mensaje y capturamos el dato ingresado desde la consola
+
+			Console.WriteLine("Ingrese el número de pisos: ");
+			pisos = Convert.ToInt16(Console.ReadLine());
+
+			/* declaramos el primer ciclo forque va a recorrer dependiendo el dato ingresado en pisos */
+			for(int i = 0; i < pisos; i++)
+			{
+				int[] pascal = new int[i]; //esto es lo alto que será el triangulo
+
+				//Se ejecuta el segundo for j que se decrementa para formar el triangulo
+				for(int j = pisos; j < i; j--)
+				{
+					Console.Write(" ");
+				}
+
+				//Ciclo for que genera las suma de las cifras
+				for(int k = 0; k < i;k++)
+				{
+
+					//se agrega una condicion ue evaluará la variable del ciclo for k incial
+					if (k == 0 || k == (i - 1))
+					{
+						pascal[k] = 1;
+					}
+					else
+					{
+						//sumamos los numeros de cada posicion del arreglo para formar el triangulo
+						pascal[k] = arreglo[k] + arreglo[k - 1];
+					}
+
+					Console.Write("[" + pascal[k] + "]");
+				} //OJITO que al salir del for que suma las cifras, k vuelve a iterarse con valor k = 0;
+
+
+				arreglo = pascal;
+				Console.WriteLine(" ");
+
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
+// ten en cuenta, para entender el ejercicio
+// arreglo[i] = son las colummnas (vertical) de la matriz, este arreglo completo va a lo horizontal
+// pascal[k] = son las columnas en una fila i especifica,
+// pascal [i] = son las filas (horizontal) de la matriz, este arreglo completo va a lo vertical
+// es medio confuso pero dibujandolo a lapiz y papel se entiende
+
